09574c8054c181b32af9a2f3c37272c783afceea
berylhu43 <beryl.hu@yahoo.com>
2023-09-09 02:28:46 -0500

    add two films

--- public/script2.js
+++ public/script2.js
@@ -1,25 +1,51 @@
 
-let currentImageIndex = 0;
+let currentImageIndex1 = 0;
+let currentImageIndex2 = 0;
 
-const images = [
-    './images/film1.png', 
+const images1 = [
+    './images/film4.png', 
     './images/film2.png', 
     './images/film3.png', 
-    './images/film4.png'
+    './images/film1.png'
 ];
 
-const imgElement = document.getElementById('currentImage');
+const images2 = [
+    './images/film7.png', 
+    './images/film8.png', 
+    './images/film5.png', 
+    './images/film6.png'
+];
+
+const imgElement = document.getElementById('currentImage1');
 
 document.getElementById('nextBtn').addEventListener('click', () => {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
+    currentImageIndex1 = (currentImageIndex1 + 1) % images1.length;
     updateImage();
 });
 
 document.getElementById('prevBtn').addEventListener('click', () => {
-    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    currentImageIndex1 = (currentImageIndex1 - 1 + images1.length) % images1.length;
     updateImage();
 });
 
 function updateImage() {
-    imgElement.src = images[currentImageIndex];
+    imgElement.src = images1[currentImageIndex1];
+}
+
+
+
+const imgElement1 = document.getElementById('currentImage2');
+
+document.getElementById('nextBtn1').addEventListener('click', () => {
+    currentImageIndex2 = (currentImageIndex2 + 1) % images2.length;
+    updateImage1();
+});
+
+document.getElementById('prevBtn1').addEventListener('click', () => {
+    currentImageIndex2 = (currentImageIndex2 - 1 + images2.length) % images2.length;
+    updateImage1();
+});
+
+function updateImage1() {
+    imgElement1.src = images2[currentImageIndex2];
 }
